f92515954ba0e96390582c289131bace368a52f0
ammar1616 <ammarmora2021@gmail.com>
2023-10-07 00:49:48 +0300

    configuring db

--- configuration/config.js
+++ configuration/config.js
@@ -1,13 +1,13 @@
 const sequelize = require("sequelize")
 
-// const objsequelize = new sequelize("u266061040_usam_event", "u266061040_usamevent_user", ">8r;qULziX", {
-//     host: "149.100.151.114",
-//     dialect: "mysql"
-// })
-
-const objsequelize = new sequelize("usam_event", "root", "", {
-    host: "localhost",
+const objsequelize = new sequelize("u266061040_usam_event", "u266061040_usamevent_user", ">8r;qULziX", {
+    host: "149.100.151.114",
     dialect: "mysql"
 })
+
+// const objsequelize = new sequelize("usam_event", "root", "", {
+//     host: "localhost",
+//     dialect: "mysql"
+// })
 
 module.exports = objsequelize
